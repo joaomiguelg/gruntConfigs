4d0cd685da29f178ae3eb8a5eb13131a76f7a3ea
João Miguel <115197583+joaomiguelg@users.noreply.github.com>
2023-05-22 22:31:12 -0300

    add grunt watch task

--- GruntFile.js
+++ GruntFile.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
         less: {
             development: {
                 files: {
-                    'main.css': 'main.less'
+                    'dev/styles/main.css': 'src/styles/main.less'
                 }
             },
             production: {
@@ -12,40 +12,26 @@ module.exports = function (grunt) {
                     compress: true,
                 },
                 files: {
-                    'main.min.css': 'main.less'
+                    'dist/styles/main.min.css': 'src/styles/main.less'
                 }
             }
 
         },
-
-        sass: {
-            dist: {
-                options: {
-                    style: 'compressed'
-                },
-                 
-                files: {
-                    'main2.css': 'main.scss'
-                }
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'],
+                tasks:['less:development']
             }
-        },
-        concurrent: {
-            target: ['olaGrunt', 'less','sass']
         }
+
+      
     })
 
-    grunt.registerTask('olaGrunt', function () {
-        const done = this.async();
-        setTimeout(function () {
-            console.log('ola Grunt');
-            done();
-        }, 5000);
-
-    })
 
     grunt.loadNpmTasks('grunt-contrib-less')
-    grunt.loadNpmTasks('grunt-contrib-sass')
-    grunt.loadNpmTasks('grunt-concurrent')
+    grunt.loadNpmTasks('grunt-contrib-watch')
+    
 
-    grunt.registerTask('default', ['concurrent'])
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less:production'])
 }
