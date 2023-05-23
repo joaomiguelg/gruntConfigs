c92aa4bf2eec74482b8062050d2325fd1f289ef8
João Miguel <115197583+joaomiguelg@users.noreply.github.com>
2023-05-22 21:07:07 -0300

    init test grunt config

---
+++ GruntFile.js
@@ -0,0 +1,51 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'main.css': 'main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'main.min.css': 'main.less'
+                }
+            }
+
+        },
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                 
+                files: {
+                    'main2.css': 'main.scss'
+                }
+            }
+        },
+        concurrent: {
+            target: ['olaGrunt', 'less','sass']
+        }
+    })
+
+    grunt.registerTask('olaGrunt', function () {
+        const done = this.async();
+        setTimeout(function () {
+            console.log('ola Grunt');
+            done();
+        }, 5000);
+
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less')
+    grunt.loadNpmTasks('grunt-contrib-sass')
+    grunt.loadNpmTasks('grunt-concurrent')
+
+    grunt.registerTask('default', ['concurrent'])
+}
